3cec28c2614b1361be936ddd0ea2730f8407f1c9
stu24801 <stu24801@gmail.com>
2021-11-13 21:13:51 +0800

    我的第一個測試用ＡＰＰ

---
+++ D2R_Info/D2R_Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D2R_Info
+//
+//  Created by Alex on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
